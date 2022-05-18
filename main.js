fe409c5ab8e54c3054358a96f31b86687bbc33f1
Jadson dos Santos Santana <jadsonunico90@gmail.com>
2022-05-17 22:44:58 -0300

    .

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 window.addEventListener('scroll', onScroll)
 
 onScroll()
-function onScroll(){
- showNavOnScroll()
- showBackToTopButtonOnScroll()
+function onScroll() {
+  showNavOnScroll()
+  showBackToTopButtonOnScroll()
 
- activateMenuAtCurrentSection(home)
- activateMenuAtCurrentSection(services)
- activateMenuAtCurrentSection(about)
- activateMenuAtCurrentSection(contact) 
+  activateMenuAtCurrentSection(home)
+  activateMenuAtCurrentSection(services)
+  activateMenuAtCurrentSection(about)
+  activateMenuAtCurrentSection(contact)
 }
 
 function activateMenuAtCurrentSection(section) {
@@ -38,42 +38,41 @@ function activateMenuAtCurrentSection(section) {
   }
 }
 
-
-function showNavOnScroll(){
-  if(scrollY > 0 ){
+function showNavOnScroll() {
+  if (scrollY > 0) {
     navigation.classList.add('scroll')
-  }else{
+  } else {
     navigation.classList.remove('scroll')
   }
 }
 
-function showBackToTopButtonOnScroll(){
-  if(scrollY > 500 ){
+function showBackToTopButtonOnScroll() {
+  if (scrollY > 550) {
     backToTopButton.classList.add('show')
-  }else{
+  } else {
     backToTopButton.classList.remove('show')
   }
 }
 
-function openMenu(){
+function openMenu() {
   document.body.classList.add('menu-expanded')
 }
 
-function closeMenu(){
+function closeMenu() {
   document.body.classList.remove('menu-expanded')
 }
 
 ScrollReveal({
   origin: 'top',
-  distance:'30px',
-  duration: 700, 
-  }).reveal(`
-   #home,
-   #home img,
-   #home .stats,
-   #services header,
-   #services .card,
-   #about,
-   #about header,
-   about .content` );
-
+  distance: '30px',
+  duration: 700
+}).reveal(`
+  #home, 
+  #home img, 
+  #home .stats, 
+  #services,
+  #services header,
+  #services .card
+  #about, 
+  #about header, 
+  #about .content`)
